25b757bd2931b5c7e4910b953380653b9b729f62
leobar37_2 <leonbarbozaelmerjoselito@gmail.com>
2021-08-19 22:58:07 -0500

    Auto-commit l3kf8l

--- scripts/create_tutorial.sh
+++ scripts/create_tutorial.sh
@@ -1,10 +1,13 @@
 # !bin/bash
 
 set -e
+## remove spaces
 
-git checkout -b $1
+text=$(echo $1 | tr -d ' ')
+
+git checkout -b $text
 rm README.md
 echo "# ${1} tutorial 🤙" > README.md
 git add .
 git commit -m "$1 tutorial init :)"
-
+echo "Tutorial $1 has been created"
