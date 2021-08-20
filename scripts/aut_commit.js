a96ea763b74632d4765ded121e34dd3fd077e6f9
leobar37_2 <leonbarbozaelmerjoselito@gmail.com>
2021-08-19 22:35:15 -0500

    Auto-commit w10h5

--- scripts/aut_commit.js
+++ scripts/aut_commit.js
@@ -10,7 +10,7 @@ const shell = require("shelljs");
 const ram = () => (Math.random() + 1).toString(36).substring(7);
 
 shell.exec("git add .");
-const dd = shell.exec(`git commit -am "Auto-commit ${ram()}"`);
+const dd = shell.exec(`git commit -am "Auto-commit ${ram()}"`).code;
 
 if (!dd) {
   console.log("commit result " + dd);
